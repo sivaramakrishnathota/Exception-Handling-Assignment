e2410903c603ae4eb35a5f7475368291fdcb5a63
sivaramakrishnathota <134401829+sivaramakrishnathota@users.noreply.github.com>
2023-08-01 20:18:12 +0530

    Delete SignUpAndLoginApp directory

--- SignUpAndLoginApp/BusinessLayer/IBLL.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BusinessLayer
-{
-    public interface IBLL
-    {
-        bool UserLogin(string email, string password);
-        bool UserRegistration(string email, string password, string userName, string mobileNumber);
-        bool ChangePassword(string email, string newpassword, string username);
-    }
-}
